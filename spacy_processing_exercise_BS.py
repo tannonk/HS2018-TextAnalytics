23cd4168a70388930f41effafea488fbfb66af82
tannonk <tannon.kew@gmail.com>
2018-10-15 10:57:36 +0200

    Update spacy_processing_exercise_BS.py

--- spacy_processing_exercise_BS.py
+++ spacy_processing_exercise_BS.py
@@ -1,5 +1,8 @@
-
+# !/usr/bin/env python3
 # coding: utf-8
+
+## Idea from https://www.youtube.com/watch?v=ZkAFJwi-G98&list=PL5vqZCoxt6JgQtFSRivfvVxNL8_U5Aa7k
+
 ## Install necessary dependencies.
 ##
 ## scpacy (https://spacy.io/usage/#section-quickstart):
@@ -12,17 +15,13 @@
 ##     $ pip install --upgrade gensim
 ## or, if you use conda:
 ##     $ conda install -c conda-forge gensim
-# In[13]:
-
 
 import re
 import spacy
 from gensim import corpora # necessary for building dictionary and bag of words representation
 from gensim import models # necessary for Phrases, LDA model and TFIDF
 
-## If there's time, could also work out how to visualise the results with PyLDA
-# In[14]:
-
+## If time, could also work out how to visualise the results with PyLDA
 
 corpus = "snf_html_corpus_simple.txt"
 
@@ -43,52 +42,36 @@ with open(corpus, "r", encoding="utf8") as inf:
 # raw_corpus
 
 ## Corpus with no preprocessing:
-# In[15]:
-
 
 corpus = [doc.split() for doc in raw_corpus]
 
 ## Have a look at Spacy's English language model - https://spacy.io/models/en#section-en_core_web_md
-# In[16]:
-
 
 nlp = spacy.load("en")
 
-# testing on one doc:
-
+## testing Spacy's English language model on one doc:
 # pro_doc = nlp(docs[0])
 # print(pro_doc)
-
-# Inspect entities found
-# pro_doc.ents
 
 # for token in pro_doc[:100]:
 #     print(token.text, token.lemma_, token.pos_, token.tag_,
 #           token.dep_, token.shape_, token.is_alpha, token.is_stop)
-# In[17]:
-
 
 trained_phrases = models.phrases.Phrases(corpus) # train a phrase model
 phraser = models.phrases.Phraser(trained_phrases) # initialise the trained phraser model
 corpus_phrased = [phraser[text] for text in corpus] # apply phraser model to each text in corpus
+
 # inspect the first text in the corpus after applying the phrase model
 # corpus_phrased[0]
 
-
-# In[18]:
-
-
 dictionary = corpora.Dictionary(corpus_phrased)
 mapped_corpus = [dictionary.doc2bow(doc) for doc in corpus_phrased]
+
 # inspect mapped_corpus
 # corpus[1][0:10]
 
-
-# In[19]:
-
-
 def print_topics_gensim(topic_model, total_topics=1, weight_threshold=0.0001, num_terms=None):
-    """Adapted pretty print topic model results from Sakar (2016) Text Analytics With Python."""
+    """Adapted pretty print topic model results from Sarkar (2016) Text Analytics With Python."""
     for index in range(total_topics):
         topic = topic_model.show_topic(index)
         topic = [(word, round(wt,4)) for word, wt in topic if abs(wt) >= weight_threshold]
@@ -97,7 +80,7 @@ def print_topics_gensim(topic_model, total_topics=1, weight_threshold=0.0001, nu
         print()
 
 def train_lda_model_gensim(corpus, total_topics=2):
-    """Adapted pretty print topic model results from Sakar (2016) Text Analytics With Python."""
+    """Adapted pretty print topic model results from Sarkar (2016) Text Analytics With Python."""
     dictionary = corpora.Dictionary(corpus)
     mapped_corpus = [dictionary.doc2bow(text) for text in corpus]
     tfidf = models.TfidfModel(mapped_corpus)
@@ -105,17 +88,8 @@ def train_lda_model_gensim(corpus, total_topics=2):
     lda = models.LdaModel(corpus_tfidf, id2word=dictionary, iterations=1000, num_topics=total_topics)
     return lda
 
-
-# In[20]:
-
-
 def run_model_for_X_topics(X=2):
     lda_gensim = train_lda_model_gensim(corpus_phrased, total_topics=X)
     print_topics_gensim(topic_model=lda_gensim, total_topics=X, num_terms=10)
 
-
-# In[21]:
-
-
 run_model_for_X_topics(10)
-
